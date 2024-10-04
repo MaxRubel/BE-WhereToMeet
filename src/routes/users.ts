4c0e197cd54de683bc57acf84c5028ab560d3120
Max Rubel <maxrubel1@gmail.com>
2024-10-04 15:18:41 -0500

    added mongo connection + create user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,29 @@
 import express from "express";
+import { Request, Response } from 'express';
+import { db } from "../index";
+import { User } from "../../dataTypes"
+
 const userRouter = express.Router();
 
-userRouter.get("/", async (req: any, res: any) => {
+/**
+ * Creates a new user to the DB
+ */
+userRouter.post("/", async (req: Request, res: Response) => {
   try {
-    //do get shit
+    const result = await db.collection<User>('users').insertOne(req.body);
+    res.status(200).json({ _id: result.insertedId, ...req.body });
+    console.log("POST: Create User")
+  } catch (err: any) {
+    console.error(err)
+    res.status(500).json({ message: err.message });
+  }
+});
+
+userRouter.get("/", async (req: Request, res: Response) => {
+  try {
     res.status(200).json({ message: "users success" });
-  } catch (err) {
-    // res.status(500).json({ message: err.message });
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
   }
 });
 
@@ -27,5 +44,6 @@ userRouter.delete("/:id", async (req: any, res: any) => {
     // res.status(500).json({ message: err.message });
   }
 });
+
 
 export default userRouter;
